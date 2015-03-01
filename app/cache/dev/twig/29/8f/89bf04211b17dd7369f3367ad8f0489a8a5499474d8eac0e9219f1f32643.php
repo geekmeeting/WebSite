6e4d8f0bbd7a04a1f286aa58d3661e9aa310a538
lourdix <lourdix@MacBook-Pro-de-lourdix.local>
2015-03-01 17:41:44 +0100

    initialisation du projet website

---
+++ app/cache/dev/twig/29/8f/89bf04211b17dd7369f3367ad8f0489a8a5499474d8eac0e9219f1f32643.php
@@ -0,0 +1,36 @@
+<?php
+
+/* TwigBundle:Exception:exception.atom.twig */
+class __TwigTemplate_298f89bf04211b17dd7369f3367ad8f0489a8a5499474d8eac0e9219f1f32643 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $this->env->loadTemplate("TwigBundle:Exception:exception.xml.twig")->display(array_merge($context, array("exception" => (isset($context["exception"]) ? $context["exception"] : $this->getContext($context, "exception")))));
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:exception.atom.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ app/cache/dev/twig/57/ff/c4672b0d071dba55bb0f87ad42f018f52e7c7d059206a31decd93f138064.php
@@ -0,0 +1,36 @@
+<?php
+
+/* TwigBundle:Exception:exception.rdf.twig */
+class __TwigTemplate_57ffc4672b0d071dba55bb0f87ad42f018f52e7c7d059206a31decd93f138064 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $this->env->loadTemplate("TwigBundle:Exception:exception.xml.twig")->display(array_merge($context, array("exception" => (isset($context["exception"]) ? $context["exception"] : $this->getContext($context, "exception")))));
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:exception.rdf.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
